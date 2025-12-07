27cb536915fd74dede78f527a97b7f7a3bcf575c
Tiberja <s_guenduez24@stud.hwr-berlin.de>
2025-12-07 13:31:17 +0000

    flask importiert

---
+++ app.py
@@ -0,0 +1,30 @@
+from flask import Flask,
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return "SkinSense - Home Screen (placeholder)"
+
+@app.route('/login')
+def login():
+    return "Login Screen (placeholder)"
+
+@app.route('/register')
+def register():
+    return "Register Screen (placeholder)"    
+
+@app.route('/skin-type')
+def skin_type():
+    return "Select Skin Type Screen (Placeholder)"   
+
+@app.route('/products')
+def products():
+    return "Product List Screen (placeholder)"  
+
+@app.route('/favorites')
+def favorites():
+    return "Favorites Screen (placeholder)"       
+
+if __name__ == "__main__":
+    app.run(debug=True)     
