e8ab8da7396d7f9bf6f631943bdffa03d3e9ce50
Acee-33 <acelya.calin.2005@gmail.com>
2026-01-17 00:49:49 +0100

    routes

--- app.py
+++ app.py
@@ -85,8 +85,8 @@ def products():
     if 'user_email' not in session:
         return redirect(url_for('login'))
 
-    if 'skin_type' not in session:
-        return redirect(url_for('skin_type'))
+   # if 'skin_type' not in session:
+   #     return redirect(url_for('skin_type'))
 
     return render_template("products.html", skin_type=session['skin_type'])
 
